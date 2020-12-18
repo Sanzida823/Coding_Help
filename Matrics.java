cd5289c3d6acbc2b9793b8636b304dee27c2e012
Sanzida <60262040+Sanzida823@users.noreply.github.com>
2020-12-19 02:50:20 +0600

    Matrics.java file added

---
+++ Matrics.java
@@ -0,0 +1,10 @@
+
+package VSM;
+
+public class Matrics {
+   public void matrics(){
+       System.out.println("Class_Count");
+       System.out.println("Method_Count");
+       System.out.println("TotalLine");
+   } 
+}
